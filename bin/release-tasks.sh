38a211c1ad720b62d97ced88ada90eac694b93f0
Tac Tacelosky <tacman@gmail.com>
2021-01-29 10:54:38 -0500

    initial version

---
+++ bin/release-tasks.sh
@@ -0,0 +1,8 @@
+#wget https://github.com/collectiveaccess/providence/archive/1.7.9.zip && unzip 1.7.9.zip -d public
+#https://github.com/collectiveaccess/providence/archive/1.7.9.zip
+#git clone --depth 1 git@github.com:collectiveaccess/providence public/providence
+#cp config/providence-setup.php public/providence-1.7.9/setup.php
+rm -Rf app/tmp
+mkdir -p media/collectiveaccess
+chmod +rw media/collectiveaccess
+
